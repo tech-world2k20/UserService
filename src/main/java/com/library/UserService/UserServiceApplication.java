7949814bb24ed28d7140bd72102a7b4a34277c2d
Abhishek <meabhibarawal@gmail.com>
2024-11-10 13:53:32 +0530

    added base mapping for "/"

--- src/main/java/com/library/UserService/UserServiceApplication.java
+++ src/main/java/com/library/UserService/UserServiceApplication.java
@@ -2,9 +2,17 @@ package com.library.UserService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class UserServiceApplication {
+
+	@GetMapping("/")
+	public String welcome(){
+		return "Welcome";
+	}
 
 
 	// change done from remove repo
